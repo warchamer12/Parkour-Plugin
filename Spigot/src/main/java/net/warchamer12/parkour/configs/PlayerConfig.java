eb2a264473511a6b9bd5161c863581367b84ad23
BuildTools <unconfigured@null.spigotmc.org>
2019-05-17 12:14:14 +0200

    Parkour-Plugin

---
+++ Spigot/src/main/java/net/warchamer12/parkour/configs/PlayerConfig.java
@@ -0,0 +1,60 @@
+package net.warchamer12.parkour.configs;
+
+import net.warchamer12.parkour.Parkour;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PlayerConfig {
+
+    static File cfile;
+    static FileConfiguration config;
+    static File folder = new File(Parkour.getPlugin().getDataFolder(), "players" + File.separator);
+    static File df = Parkour.getPlugin().getDataFolder();
+
+    public static void create(Player player) {
+        cfile = new File(df, "players" + File.separator + player.getName() + ".yml");
+        if (!df.exists()) df.mkdir();
+        if (!cfile.exists()) {
+            try {
+                cfile.createNewFile();
+            } catch (IOException e) {
+                Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Blad przy tworzeniu pliku " + cfile.getName() + "!");
+            }
+        }
+        config = YamlConfiguration.loadConfiguration(cfile);
+
+    }
+
+    public  static File getFolder() {
+        return folder;
+    }
+
+    public  static File getFile() {
+        return cfile;
+    }
+
+    public static  void load(Player player) {
+        cfile = new File(df, "players" + File.separator + player.getName() + ".yml");
+        config = YamlConfiguration.loadConfiguration(cfile);
+    }
+
+    public static  FileConfiguration get() {
+        return config;
+    }
+
+    public static void save() {
+        try {
+            config.save(cfile);
+        } catch (Exception event) {
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Blad przy zapisywaniu pliku " + cfile.getName() + "!");
+        }
+    }
+
+
+}
--- Spigot/src/main/java/net/warchamer12/parkour/listeners/PlayerJoinListener.java
+++ Spigot/src/main/java/net/warchamer12/parkour/listeners/PlayerJoinListener.java
@@ -1,29 +1,24 @@
 package net.warchamer12.parkour.listeners;
 
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-
-import net.warchamer12.parkour.Parkour;
-import net.warchamer12.parkour.utils.Util;
-
+import net.warchamer12.parkour.configs.PlayerConfig;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class PlayerJoinListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        new BukkitRunnable() {
-
-            @Override
-            public void run() {
-                player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new ComponentBuilder(Util.fixColor("&4Siema stary pijany!")).create());
-            }
-        }.runTaskTimer(Parkour.getPlugin().getInstance(), 20L, 20L);
+        if (PlayerConfig.getFile() == player) {
+            return;
+        } else {
+            Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "Utworzono plik " + player + ".yml" + "!");
+            PlayerConfig.create(player);
+        }
     }
 
 

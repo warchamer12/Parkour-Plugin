f0e3d566dd95df9410c4b17356959387394622fa
BuildTools <unconfigured@null.spigotmc.org>
2019-05-17 12:36:39 +0200

    Parkour-Plugin

--- Spigot/src/main/java/net/warchamer12/parkour/listeners/PlayerJoinListener.java
+++ Spigot/src/main/java/net/warchamer12/parkour/listeners/PlayerJoinListener.java
@@ -1,20 +1,19 @@
 package net.warchamer12.parkour.listeners;
 
 import net.warchamer12.parkour.configs.PlayerConfig;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-
-import java.io.File;
 
 public class PlayerJoinListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        if (PlayerConfig.getFile().getName() == File.separator + player.getUniqueId()) {
-            return;
+        if (player.hasPlayedBefore()) {
+            PlayerConfig.load(player);
         } else {
             PlayerConfig.create(player);
             PlayerConfig.save();

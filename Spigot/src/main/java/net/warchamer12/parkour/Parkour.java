45c606abffb16b9cd5552e0ceafa662da0f6dd42
BuildTools <unconfigured@null.spigotmc.org>
2019-05-17 15:11:24 +0200

    Parkour-Plugin

--- Spigot/src/main/java/net/warchamer12/parkour/Parkour.java
+++ Spigot/src/main/java/net/warchamer12/parkour/Parkour.java
@@ -7,6 +7,7 @@ import net.warchamer12.parkour.configs.area.ParkourConfigHard;
 import net.warchamer12.parkour.configs.area.ParkourConfigMedium;
 import net.warchamer12.parkour.configs.area.ParkourConfigUltra;
 import net.warchamer12.parkour.listeners.PlayerJoinListener;
+import net.warchamer12.parkour.objects.ParkourObject;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
@@ -18,6 +19,7 @@ public final class Parkour extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        plugin.saveDefaultConfig();
         plugin = this;
         plugin.getDataFolder().getPath();
         getLogger().info("Starting parkour!");
@@ -33,13 +35,19 @@ public final class Parkour extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("Disabled plugin!");
+        //Players
         PlayerConfig.save();
 
         //Parkours
         ParkourConfigEasy.save();
+        Parkour.getPlugin().getInstance().getConfig().set("Easy", ParkourObject.getEasy());
         ParkourConfigMedium.save();
+        Parkour.getPlugin().getInstance().getConfig().set("Medium", ParkourObject.getMedium());
         ParkourConfigHard.save();
+        Parkour.getPlugin().getInstance().getConfig().set("Hard", ParkourObject.getHard());
         ParkourConfigUltra.save();
+        Parkour.getPlugin().getInstance().getConfig().set("Ultra", ParkourObject.getUltra());
+        plugin.saveConfig();
     }
 
     private void getCommands() {

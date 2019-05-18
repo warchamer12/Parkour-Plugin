d40d424d451f26da67a10843d471199040f2aef7
BuildTools <unconfigured@null.spigotmc.org>
2019-05-18 12:07:25 +0200

    Parkour-Plugin

--- Spigot/src/main/java/net/warchamer12/parkour/listeners/PlayerJoinListener.java
+++ Spigot/src/main/java/net/warchamer12/parkour/listeners/PlayerJoinListener.java
@@ -7,7 +7,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class PlayerJoinListener implements Listener {
+
+    Date joined = new Date();
+    SimpleDateFormat format1 = new SimpleDateFormat("dd/MM/yyyy");
+    SimpleDateFormat format2 = new SimpleDateFormat("HH:mm:ss");
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
@@ -17,6 +24,8 @@ public class PlayerJoinListener implements Listener {
         } else {
             PlayerConfig.create(player);
             PlayerConfig.get().set("playername", player.getName());
+            PlayerConfig.get().set("joined in", format1.format(joined));
+            PlayerConfig.get().set("joined at", format2.format(joined));
             PlayerConfig.save();
         }
     }
